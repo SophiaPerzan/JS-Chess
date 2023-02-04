72185c55e5ba6ede467be40e325a78da26236759
Sophia <sophia@perzan.dev>
2023-02-04 02:08:57 -0500

    Added random playing

--- scripts.js
+++ scripts.js
@@ -1,11 +1,30 @@
 
 import { Chess } from 'https://unpkg.com/chess.js@1.0.0-beta.3/dist/chess.js';
 
+const chess = new Chess();
+
 var board = Chessboard('chessBoard', {
     draggable: true,
-    dropOffBoard: 'trash',
-    sparePieces: true
-  })
-
+    dropOffBoard: 'snapback',
+    sparePieces: false,
+    position: 'start'/*,
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onSnapEnd: onSnapEnd*/
+  });
   $('#startBtn').on('click', board.start)
   $('#clearBtn').on('click', board.clear)
+
+  function makeRandomMove(){
+  if (!chess.isGameOver()) {
+    const moves = chess.moves()
+    const move = moves[Math.floor(Math.random() * moves.length)]
+    chess.move(move)
+    board.position(chess.fen())
+    }
+    if (!chess.isGameOver()) {
+        setTimeout(()=> {makeRandomMove()}, 400  );
+    }
+}
+
+makeRandomMove();
