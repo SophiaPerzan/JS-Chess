69bc97566d875d2df9d131c00b94a8e93f8c87aa
Sophia <sophia@perzan.dev>
2023-02-04 00:50:58 -0500

    Added chess.js library

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
+
+import { Chess } from 'https://unpkg.com/chess.js@1.0.0-beta.3/dist/chess.js';
+
 var board = Chessboard('chessBoard', {
     draggable: true,
     dropOffBoard: 'trash',
     sparePieces: true
   })
-  
+
   $('#startBtn').on('click', board.start)
   $('#clearBtn').on('click', board.clear)
