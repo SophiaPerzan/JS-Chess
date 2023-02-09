f04700ddc65b379d7a5ccbc70ec13f24de28766c
Sophia <sophia@perzan.dev>
2023-02-08 23:50:54 -0500

    Added last move highlighting

---
+++ engine.js
@@ -0,0 +1,17 @@
+//bitboards - one for each piece & colour combo
+//attack bitboards (magic numbers)
+//psuedo legal move generation
+//
+
+let whitePawns = 0
+let whiteKnights = 0
+let whiteBishops = 0
+let whiteRooks = 0
+let whiteQueens = 0
+let whiteKings = 0
+let blackPawns = 0
+let blackKnights = 0
+let blackBishops = 0
+let blackRooks = 0
+let blackQueens = 0
+let blackKings = 0
