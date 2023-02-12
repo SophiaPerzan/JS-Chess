4df394533adc3330857e1cddefd0b72b19ddc3da
Sophia <sophia@perzan.dev>
2023-02-11 21:34:39 -0500

    Added minimax and alpha-beta pruning

--- engine.js
+++ engine.js
@@ -1,40 +1,116 @@
-//bitboards - one for each piece & colour combo
-//attack bitboards (magic numbers)
-//psuedo legal move generation
-//
+import { chess } from "./scripts.js"
 
-/*let whitePawns = 0
-let whiteKnights = 0
-let whiteBishops = 0
-let whiteRooks = 0
-let whiteQueens = 0
-let whiteKings = 0
-let blackPawns = 0
-let blackKnights = 0
-let blackBishops = 0
-let blackRooks = 0
-let blackQueens = 0
-let blackKings = 0
+const whitePawnPieceSquareTable = [ 0,  0,  0,  0,  0,  0,  0,  0,
+                                 50, 50, 50, 50, 50, 50, 50, 50,
+                                 10, 10, 20, 30, 30, 20, 10, 10,
+                                  5,  5, 10, 25, 25, 10,  5,  5,
+                                  0,  0,  0, 20, 20,  0,  0,  0,
+                                  5, -5,-10,  0,  0,-10, -5,  5,
+                                  5, 10, 10,-20,-20, 10, 10,  5,
+                                  0,  0,  0,  0,  0,  0,  0,  0]
 
-const RANK1 = 0xff
-const RANK2 = 0xff00
-const RANK3 = 0xff0000
-const RANK4 = 0xff000000
-const RANK5 = 0xff00000000
-const RANK6 = 0xff0000000000
-const RANK7 = 0xff000000000000
-const RANK8 = 0xff00000000000000
+const whiteKnightPieceSquareTable = [-50,-40,-30,-30,-30,-30,-40,-50,
+                                 -40,-20,  0,  0,  0,  0,-20,-40,
+                                 -30,  0, 10, 15, 15, 10,  0,-30,
+                                 -30,  5, 15, 20, 20, 15,  5,-30,
+                                 -30,  0, 15, 20, 20, 15,  0,-30,
+                                 -30,  5, 10, 15, 15, 10,  5,-30,
+                                 -40,-20,  0,  5,  5,  0,-20,-40,
+                                 -50,-40,-30,-30,-30,-30,-40,-50] 
 
-const AFILE = 0x0101010101010101
-const BFILE = 0x0202020202020202
-const CFILE = 0x0303030303030303
-const DFILE = 0x0404040404040404
-const EFILE = 0x0505050505050505
-const FFILE = 0x0606060606060606
-const GFILE = 0x0707070707070707
-const HFILE = 0x0808080808080808*/
+const whiteBishopPieceSquareTable = [-20,-10,-10,-10,-10,-10,-10,-20,
+                                     -10,  0,  0,  0,  0,  0,  0,-10,
+                                     -10,  0,  5, 10, 10,  5,  0,-10,
+                                     -10,  5,  5, 10, 10,  5,  5,-10,
+                                     -10,  0, 10, 10, 10, 10,  0,-10,
+                                     -10, 10, 10, 10, 10, 10, 10,-10,
+                                     -10,  5,  0,  0,  0,  0,  5,-10,
+                                     -20,-10,-10,-10,-10,-10,-10,-20]
 
-function generateMovesToDepth(depth){
+const whiteRookPieceSquareTable = [  0,  0,  0,  0,  0,  0,  0,  0,
+                                5, 10, 10, 10, 10, 10, 10,  5,
+                               -5,  0,  0,  0,  0,  0,  0, -5,
+                               -5,  0,  0,  0,  0,  0,  0, -5,
+                               -5,  0,  0,  0,  0,  0,  0, -5,
+                               -5,  0,  0,  0,  0,  0,  0, -5,
+                               -5,  0,  0,  0,  0,  0,  0, -5,
+                                0,  0,  0,  5,  5,  0,  0,  0]
+                            
+const whiteQueenPieceSquareTable = [-20,-10,-10, -5, -5,-10,-10,-20,
+                               -10,  0,  0,  0,  0,  0,  0,-10,
+                               -10,  0,  5,  5,  5,  5,  0,-10,
+                                -5,  0,  5,  5,  5,  5,  0, -5,
+                                 0,  0,  5,  5,  5,  5,  0, -5,
+                               -10,  5,  5,  5,  5,  5,  0,-10,
+                               -10,  0,  5,  0,  0,  0,  0,-10,
+                               -20,-10,-10, -5, -5,-10,-10,-20]
+
+const whiteKingPieceSquareTable = [-30,-40,-40,-50,-50,-40,-40,-30,
+                                   -30,-40,-40,-50,-50,-40,-40,-30,
+                                   -30,-40,-40,-50,-50,-40,-40,-30,
+                                   -30,-40,-40,-50,-50,-40,-40,-30,
+                                   -20,-30,-30,-40,-40,-30,-30,-20,
+                                   -10,-20,-20,-20,-20,-20,-20,-10,
+                                    20, 20,  0,  0,  0,  0, 20, 20,
+                                    20, 30, 10,  0,  0, 10, 30, 20]
+
+const blackPawnPieceSquareTable = [ 0,  0,  0,  0,  0,  0,  0,  0,
+                                 5, 10, 10,-20,-20, 10, 10,  5,
+                                 5, -5,-10,  0,  0,-10, -5,  5,
+                                  0,  0,  0, 20, 20,  0,  0,  0,
+                                  5,  5, 10, 25, 25, 10,  5,  5,
+                                  10, 10, 20, 30, 30, 20, 10, 10,
+                                  50, 50, 50, 50, 50, 50, 50, 50,
+                                  0,  0,  0,  0,  0,  0,  0,  0]
+
+const blackKnightPieceSquareTable = [-50,-40,-30,-30,-30,-30,-40,-50,
+                                 -40,-20,  0,  5,  5,  0,-20,-40,
+                                 -30,  5, 10, 15, 15, 10,  5,-30,
+                                 -30,  0, 15, 20, 20, 15,  0,-30,
+                                 -30,  5, 15, 20, 20, 15,  5,-30,
+                                 -30,  0, 10, 15, 15, 10,  0,-30,
+                                 -40,-20,  0,  0,  0,  0,-20,-40,
+                                 -50,-40,-30,-30,-30,-30,-40,-50] 
+
+const blackBishopPieceSquareTable = [-20,-10,-10,-10,-10,-10,-10,-20,
+                                     -10,  5,  0,  0,  0,  0,  5,-10,
+                                     -10, 10, 10, 10, 10, 10, 10,-10,
+                                     -10,  0, 10, 10, 10, 10,  0,-10,
+                                     -10,  5,  5, 10, 10,  5,  5,-10,
+                                     -10,  0,  5, 10, 10,  5,  0,-10,
+                                     -10,  0,  0,  0,  0,  0,  0,-10,
+                                     -20,-10,-10,-10,-10,-10,-10,-20]
+
+const blackRookPieceSquareTable = [  0,  0,  0,  5,  5,  0,  0,  0,
+                                -5,  0,  0,  0,  0,  0,  0, -5,
+                               -5,  0,  0,  0,  0,  0,  0, -5,
+                               -5,  0,  0,  0,  0,  0,  0, -5,
+                               -5,  0,  0,  0,  0,  0,  0, -5,
+                               -5,  0,  0,  0,  0,  0,  0, -5,
+                               5, 10, 10, 10, 10, 10, 10,  5,
+                               0,  0,  0,  0,  0,  0,  0,  0]
+                            
+const blackQueenPieceSquareTable = [-20,-10,-10, -5, -5,-10,-10,-20,
+                               -10,  0,  5,  0,  0,  0,  0,-10,
+                               -10,  5,  5,  5,  5,  5,  0,-10,
+                                0,  0,  5,  5,  5,  5,  0, -5,
+                                -5,  0,  5,  5,  5,  5,  0, -5,
+                                -10,  0,  5,  5,  5,  5,  0,-10,
+                                -10,  0,  0,  0,  0,  0,  0,-10,
+                               -20,-10,-10, -5, -5,-10,-10,-20]
+
+const blackKingPieceSquareTable = [20, 30, 10,  0,  0, 10, 30, 20,
+                                   20, 20,  0,  0,  0,  0, 20, 20,
+                                   -10,-20,-20,-20,-20,-20,-20,-10,
+                                   -20,-30,-30,-40,-40,-30,-30,-20,
+                                   -30,-40,-40,-50,-50,-40,-40,-30,
+                                   -30,-40,-40,-50,-50,-40,-40,-30,
+                                   -30,-40,-40,-50,-50,-40,-40,-30,
+                                    -30,-40,-40,-50,-50,-40,-40,-30]
+
+
+
+export function generateMovesToDepth(depth){
     if(depth<1){
       return 1
     }
@@ -47,3 +123,161 @@ function generateMovesToDepth(depth){
     }
     return numberofNodes
   }
+
+function maximizing(depth, alpha, beta){
+    if(depth === 0){
+        return evaltuatePosition()
+    }else{
+        const moves = chess.moves()
+        for (let i=0;i<moves.length;i++){
+            chess.move(moves[i])
+            let score = minimizing(depth-1, alpha, beta)
+            if(score>=beta){
+                chess.undo()
+                return beta
+            }
+            if(score>alpha){
+                alpha = score
+            }
+            chess.undo()
+          }
+          return alpha
+    }
+}
+
+function minimizing(depth, alpha, beta){
+    if(depth === 0){
+        return evaltuatePosition()
+    }else{
+        const moves = chess.moves()
+        for (let i=0;i<moves.length;i++){
+            chess.move(moves[i])
+            let score = maximizing(depth-1, alpha, beta)
+            if(score<=alpha){
+                chess.undo()
+                return alpha
+            }
+            if(score<beta){
+                beta = score
+            }
+            chess.undo()
+          }
+        return beta
+    }
+}
+
+function iterativeDeepening(depth){
+
+    
+}
+
+export function getBestMove(depth){
+    const moves = chess.moves()
+    let moveScores = []
+    for (let i=0;i<moves.length;i++){
+        chess.move(moves[i])
+        if(chess.turn() === 'w'){
+            moveScores.push(maximizing(depth-1, -1000000000, 1000000000))
+        }else{
+            moveScores.push(minimizing(depth-1, -1000000000, 1000000000))
+        }
+        chess.undo()
+      }
+    let bestMoveIndex = 0
+    
+    if(chess.turn() === 'w'){
+        let max = -100000000
+        for (let i=0;i<moveScores.length;i++){
+            if(moveScores[i]>max){
+                max = moveScores[i]
+                bestMoveIndex = i
+            }
+        }
+    }else{
+        let min = 100000000
+        for (let i=0;i<moveScores.length;i++){
+            if(moveScores[i]<min){
+                min = moveScores[i]
+                bestMoveIndex = i
+            }
+        }
+    }
+    return {move: moves[bestMoveIndex], score: moveScores[bestMoveIndex]}
+}
+
+export function evaltuatePosition(){
+    let sumEval = 0
+    let squareEval = 0;
+    if(chess.isDraw()){
+        return 0
+    }
+    let boardArray = chess.board()
+    for(let i=0;i<8;i++){
+        for(let j=0;j<8;j++){
+            if(boardArray[i][j] === null){
+                
+            }else{
+                squareEval = 0
+                
+                switch(boardArray[i][j].color){
+                    case('w'):
+                    switch(boardArray[i][j].type){
+                        case('p'):
+                        squareEval = 100 + whitePawnPieceSquareTable[i*8+j]
+                        break;
+                        case('n'):
+                        squareEval = 300 + whiteKnightPieceSquareTable[i*8+j]
+                        break;
+                        case('b'):
+                        squareEval = 325 + whiteBishopPieceSquareTable[i*8+j]
+                        break;
+                        case('r'):
+                        squareEval = 500 + whiteRookPieceSquareTable[i*8+j]
+                        break;
+                        case('q'):
+                        squareEval = 900 + whiteQueenPieceSquareTable[i*8+j]
+                        break;
+                        case('k'):
+                        squareEval = 1000000 + whiteKingPieceSquareTable[i*8+j]
+                        break;
+                        default:
+                        squareEval = 0
+                        break;
+                    }
+                    sumEval+=squareEval
+                    break;
+                    case('b'):
+                    switch(boardArray[i][j].type){
+                        case('p'):
+                        squareEval = 100 + blackPawnPieceSquareTable[i*8+j]
+                        break;
+                        case('n'):
+                        squareEval = 300 + blackKnightPieceSquareTable[i*8+j]
+                        break;
+                        case('b'):
+                        squareEval = 325 + blackBishopPieceSquareTable[i*8+j]
+                        break;
+                        case('r'):
+                        squareEval = 500 + blackRookPieceSquareTable[i*8+j]
+                        break;
+                        case('q'):
+                        squareEval = 900 + blackQueenPieceSquareTable[i*8+j]
+                        break;
+                        case('k'):
+                        squareEval = 1000000 + blackKingPieceSquareTable[i*8+j]
+                        break;
+                        default:
+                        squareEval = 0
+                        break;
+                    }
+                    sumEval-=squareEval
+                    break;
+                    default:
+                    break;
+                }
+            }
+            
+        }
+    }
+    return sumEval
+}
